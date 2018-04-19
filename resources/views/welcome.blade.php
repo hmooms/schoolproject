50e044ebeab8620d6f2988459d4268eb13763848
Hugo Ooms <99046763@mydavinci.nl>
2018-04-19 15:45:14 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MusicShare
                 </div>
 
                 <div class="links">
